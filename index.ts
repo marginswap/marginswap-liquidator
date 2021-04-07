715c794d1b1243cb0e929044f5f84e39cea80c07
Vincent Raerek <vincent@monadnomad.dev>
2021-04-06 17:10:18 -0700

    Add main script

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { List } from 'immutable';
+import _ from 'lodash';
+import Web3 from 'web3';
+import fetch from 'node-fetch';
+import contractAddresses from '@marginswap/core-abi/addresses.json';
+
+const { ETHERSCAN_API_KEY, INFURA_KEY } = process.env;
+const CONTRACT_ADDRESS: string = contractAddresses.kovan.MarginRouter;
+const ETHERSCAN_URL = `http://api.etherscan.io/api?module=contract&action=getabi&address=${CONTRACT_ADDRESS}&apikey=${ETHERSCAN_API_KEY}`;
+const web3 = new Web3(`wss://mainnet.infura.io/ws/v3/${INFURA_KEY}`);
+
+function getContract(contractUrl: string) {
+  return fetch(contractUrl)
+    .then(resp => resp.json())
+    .then(abi => new web3.eth.Contract(abi, CONTRACT_ADDRESS));
+}
+
+async function getAccountAddresses(): Promise<List<string>> {
+  return getContract(ETHERSCAN_URL)
+    .then(contract => contract.getPastEvents('allEvents'))
+    .then(events => _.map(events, event => event.address))
+    .then(addresses => List(addresses));
+}
+
+export default function main() {
+  getAccountAddresses().then(results => _.each(results, console.log));
+}
+
+main();
