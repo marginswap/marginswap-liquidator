dc6f7163239db62e2cb16e697e8b3a20da94ab11
Vincent Raerek <vincent@monadnomad.dev>
2021-04-07 14:22:58 -0700

    Liquidate accounts

--- index.ts
+++ index.ts
@@ -9,26 +9,37 @@ dotenv.config();
 type address = string
 
 const { ETHERSCAN_API_KEY, INFURA_KEY } = process.env;
-const CONTRACT_ADDRESS: address = contractAddresses.kovan.MarginRouter;
-const ETHERSCAN_URL = `http://api-kovan.etherscan.io/api?module=contract&action=getabi&address=${CONTRACT_ADDRESS}&apikey=${ETHERSCAN_API_KEY}`;
+const MARGIN_ROUTER_ADDRESS: address = contractAddresses.kovan.MarginRouter;
+const CROSS_MARGIN_TRADING_ADDRESS: address = contractAddresses.kovan.CrossMarginTrading;
+const ETHERSCAN_BASE_URL = `http://api-kovan.etherscan.io/api?module=contract&action=getabi&apikey=${ETHERSCAN_API_KEY}`;
 const web3 = new Web3(`wss://kovan.infura.io/ws/v3/${INFURA_KEY}`);
 
-function getContract(contractUrl: address) {
+function etherscanUrl(contract: address): string {
+  return `${ETHERSCAN_BASE_URL}&address=${contract}`;
+}
+
+function getContract(contractUrl: string, contractAddr: address) {
   return fetch(contractUrl)
     .then(resp => resp.json())
     .then(result => JSON.parse(result.result))
-    .then(abi => new web3.eth.Contract(abi, CONTRACT_ADDRESS));
+    .then(abi => new web3.eth.Contract(abi, contractAddr));
 }
 
-async function getAccountAddresses(): Promise<List<address>> {
-  return getContract(ETHERSCAN_URL)
-    .then(contract => contract.getPastEvents('allEvents'))
+function getAccountAddresses(): Promise<List<address>> {
+  const url = etherscanUrl(MARGIN_ROUTER_ADDRESS);
+  return getContract(url, MARGIN_ROUTER_ADDRESS)
+    .then(marginRouter => marginRouter.getPastEvents('allEvents'))
     .then(events => Seq(events).map(event => event.address).toList());
 }
 
-export default async function main() {
-  return getAccountAddresses()
-    .then(results => results.forEach(console.log));
+function liquidateAccounts(accounts: List<address>) {
+  const url = etherscanUrl(CROSS_MARGIN_TRADING_ADDRESS);
+  return getContract(url, CROSS_MARGIN_TRADING_ADDRESS)
+    .then(crossMarginTrading => crossMarginTrading.methods.liquidate(accounts));
+}
+
+export default function main() {
+  return getAccountAddresses().then(liquidateAccounts);
 }
 
 main().then(_ => process.exit());
