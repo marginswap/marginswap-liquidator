a64bad1243630ff2f9535c3b33a2f79363edce44
Gabriel Pickard <wergomat@gmail.com>
2021-04-23 22:16:35 -0500

    a few intermediate changes

--- index.ts
+++ index.ts
@@ -3,21 +3,37 @@ import Web3 from 'web3';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 import contractAddresses from '@marginswap/core-abi/addresses.json';
+import HDWalletProvider from '@truffle/hdwallet-provider';
+import fs from 'fs';
 
 dotenv.config();
 
-type address = string
+
+type address = string;
 
 const env = process.argv[3];
 const network = env === 'prod' ? 'mainnet' : 'kovan';
+const chainIds:{[key:string]:'1' | '42'} = {
+  'kovan': '42',
+  'mainnet': '1'
+};
 const { ETHERSCAN_API_KEY, INFURA_KEY } = process.env;
-const MARGIN_ROUTER_ADDRESS: address = contractAddresses[network].MarginRouter;
-const CROSS_MARGIN_TRADING_ADDRESS: address = contractAddresses[network].CrossMarginTrading;
+const MARGIN_ROUTER_ADDRESS: address = contractAddresses[chainIds[network]].MarginRouter;
+const CROSS_MARGIN_TRADING_ADDRESS: address = contractAddresses[chainIds[network]].CrossMarginTrading;
 const ETHERSCAN_BASE_URL = network === 'mainnet'
   ? 'http://api.etherscan.io'
   : 'http://api-kovan.etherscan.io';
 const ETHERSCAN_COMMON_PARAMS = `api?module=contract&action=getabi&apikey=${ETHERSCAN_API_KEY}`;
-const web3 = new Web3(`wss://${network}.infura.io/ws/v3/${INFURA_KEY}`);
+
+
+
+const homedir = require('os').homedir();
+const privateKey = fs.readFileSync(`${homedir}/.marginswap-secret`).toString().trim();
+const provider = new HDWalletProvider({
+  privateKeys:[privateKey],
+  providerOrUrl: `https://${network}.infura.io/v3/${INFURA_KEY}`
+})
+const web3 = new Web3(provider);
 
 function etherscanUrl(contract: address): string {
   return `${ETHERSCAN_BASE_URL}/${ETHERSCAN_COMMON_PARAMS}&address=${contract}`;
@@ -56,12 +72,22 @@ function filterLiquifiable(accounts: List<address>): Promise<List<address>> {
 
 function liquidateAccounts(accounts: List<address>) {
   return getCrossMarginTrading()
-    .then(cmt => cmt.methods.liquidate(accounts));
+    .then(cmt => {
+      if (accounts.size > 0) {
+        return cmt.methods.liquidate(accounts);
+      }
+    })
+    .then(console.log);
 }
 
 export default function main() {
   return getAccountAddresses()
     .then(filterLiquifiable)
+    .then((liquifiableAccounts) => {
+      console.log(`The following accounts are liquifiable:`);
+      console.log(liquifiableAccounts);
+      return liquifiableAccounts;
+    })
     .then(liquidateAccounts);
 }
 
